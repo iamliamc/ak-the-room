8e9680a8139d682ae43424c00d768f6ac50fa279
Liam Considine <liam.considine@gusto.com>
2022-07-24 12:30:18 -0400

    Getting they layout a bit closer

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,42 +1,47 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import Link from '@mui/material/Link';
 
 
+import Box, { BoxProps } from '@mui/material/Box';
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-export default function Album() {
+function Item(props: BoxProps) {
+  const { sx, ...other } = props;
   return (
-    <div>
-      <main>
-        <Container sx={{ py: 8 }} maxWidth="md">
-          <Grid container spacing={2}>
-              <Grid item xs={12} sm={6} md={4}>
-                <video>
-                    <source src="/videos/ams_overdue.mp4" type="video/mp4"/>
+    <Box
+      sx={{
+        bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#101010' : '#fff'),
+        color: (theme) => (theme.palette.mode === 'dark' ? 'grey.300' : 'grey.800'),
+        border: '1px solid',
+        borderColor: (theme) =>
+          theme.palette.mode === 'dark' ? 'grey.800' : 'grey.300',
+        p: 1,
+        m: 1,
+        borderRadius: 2,
+        fontSize: '0.875rem',
+        fontWeight: '700',
+        ...sx,
+      }}
+      {...other}
+    />
+  );
+}
+
+export default function Test() {
+  return (
+    <div style={{ width: '100%' }}>
+        <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)' }}>
+            <Item style={{display: "flex", flexDirection: "row"}}>
+                <video controls muted>
+                    <source src="./videos/satellite.mp4" type="video/mp4"/>
                 </video>
-              </Grid>
-              <Grid item xs={12} sm={6} md={4}>
-                <video>
-                    <source src="/videos/satellite.mp4" type="video/mp4"/>
+            </Item>
+            <Item style={{display: "flex", flexDirection: "row"}}>
+                <video controls muted>
+                    <source src="./videos/ams_overdue.mp4" type="video/mp4"/>
                 </video>
-              </Grid>
-          </Grid>
-        </Container>
-      </main>
-      </div>
+            </Item>
+        </Box>
+    </div>
   );
 }
